9eceac6d563bf0b5808c38dbedabd8654cbebeba
Leszek <a.wierzchowski@proton.me>
2024-07-12 20:03:52 +0200

    First

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.example.bibliotekagier {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+
+    opens com.example.bibliotekagier to javafx.fxml;
+    exports com.example.bibliotekagier;
+}
